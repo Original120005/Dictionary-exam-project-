18c976ce8ff7fed8e77306509a14a5f22c5a88af
Дима <@DESKTOP-TC925PR>
2023-03-19 17:30:13 +0200

    Add project files.

---
+++ Dictionary(exam project)/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary_exam_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Создаем файловый поток,и записываем в него инфу из словаря
+
+            /*Dictionary dict = new Dictionary();
+            dict.Create();
+            FileStream fs = new FileStream(dict.type + ".txt", FileMode.Open);
+            StreamWriter sw = new StreamWriter(fs);
+            dict.Add(dict.dictionary, fs, sw);
+            dict.Add(dict.dictionary, fs, sw);
+            sw.Close();
+
+            dict.Delete(dict.dictionary, dict.type + ".txt");
+            dict.Edit(dict.dictionary, dict.type + ".txt");
+            dict.Print();*/
+
+
+            Dictionary dict = new Dictionary(); //Создаем экземпляр класса dictionary
+
+            Console.WriteLine("Hello,im your personal dictionary.");
+            bool exit = false;
+            while (exit != true) //while до тех пор пока пользователь не выберет exit
+            {
+                Console.Write("Choose what you need:   1.Create  2.Delete  3.Edit  4.Print  5.Search  6.Exit  Write: ");
+                try
+                {
+                    string a = Console.ReadLine();
+                    int choose = Convert.ToInt32(a);
+                    switch (choose) // реализуем свитч с 6 опциями 
+                    {
+                        case 1:
+                            dict.Create();
+                            Console.WriteLine();
+                            break;
+
+                        case 2:
+                            dict.Delete(dict.dictionary, dict.type + ".txt");
+                            Console.WriteLine();
+                            break;
+
+                        case 3:
+                            dict.Edit(dict.dictionary, dict.type + ".txt");
+                            Console.WriteLine();
+                            break;
+
+                        case 4:
+                            dict.Print();
+                            Console.WriteLine();
+                            break;
+
+                        case 5:
+                            dict.Search(dict.dictionary);
+                            Console.WriteLine();
+                            break;
+
+                        case 6:
+                            exit = true;
+                            break;
+                    }
+                }
+                catch (Exception ex) { Console.WriteLine($"\nMisstake!!\n{ex.Message}\n"); } // если возникает исключение выводим ошибку
+            }
+
+
+
+        }
+    }
+}
---
+++ Dictionary(exam project)/Properties/Dictionary.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary_exam_project
+{
+    interface CreateDictionary //Содержит 1 метод который создает новый словарь
+    {
+        void Create();
+    }
+    interface EditDictionary //Содержит 3 метода предназначеные для Удаления,Редактирования и Поиска слов в словаре
+    {
+        void Delete(Dictionary<string, string> dic, string file);
+        void Edit(Dictionary<string, string> dic, string file);
+        void Search(Dictionary<string, string> dic);
+    }
+    interface PrintDictionary  //Содержит 1 метод который выводит на экран содержимое словаря 
+    {
+        void Print();
+    }
+
+    class Dictionary : CreateDictionary, EditDictionary, PrintDictionary //Определяем класс Dictionary который содержит все 3 интерфейса
+    {
+        public Dictionary<string, string> dictionary;
+        public string type;
+
+        public Dictionary() { } //Конструктор без параметров создает экземпляр класса
+        public Dictionary(Dictionary<string, string> word) //Констуктор принимает словарь в качестве параметра и инициализирует поле dictionary
+        {
+            dictionary = word;
+        }
+
+        public void Create()  //Метод создает новый словарь запрашивая у пользователя тип словаря
+        {
+            dictionary = new Dictionary<string, string>();
+            Console.Write("Enter type of dictionary: ");
+            type = Console.ReadLine();
+            FileStream file = new FileStream(type + ".txt", FileMode.OpenOrCreate);
+            StreamWriter sw = new StreamWriter(file);
+            file.Close();
+        }
+
+        public void Delete(Dictionary<string, string> dic, string file)  //Метод позволяет удалить слово из словаря запрашивая слова которое надо удалить,после слово удаляется
+        {
+            Console.Write("Enter word to delete: ");
+            string word = Console.ReadLine();
+            var lines = File.ReadAllLines(file).ToList();
+            int index = lines.IndexOf(word + " -- " + dic[word]);
+
+            if (dic.ContainsKey(word))
+            {
+                dic.Remove(word);
+                lines.RemoveAt(index);
+                File.WriteAllLines(file, lines);
+            }
+        }
+
+        public void Edit(Dictionary<string, string> dic, string file)  //Метод позволяет редактировать слово,запрашивая слово кторое надо изменить
+        {
+            Console.Write("Enter word you want to edit: ");
+            string find = Console.ReadLine();
+            if (dic.ContainsKey(find))
+            {
+                var lines = File.ReadAllLines(file).ToList();
+                int index = lines.IndexOf(find + " -- " + dic[find]);
+
+                Console.Write("Enter new word: ");
+                string word = Console.ReadLine();
+                Console.Write("Enter new translate: ");
+                string translate = Console.ReadLine();
+                dic.Remove(find);
+                lines.RemoveAt(index);
+
+                string save = null;
+                foreach (var i in dic)
+                {
+                    save += $"{i.Key} - {i.Value}\n";
+                }
+                File.WriteAllLines($"{file}", lines);
+                dic.Add(word, translate);
+                string text = null;
+                foreach (var i in dic)
+                {
+                    text += $"{i.Key} - {i.Value}\n";
+                }
+                File.WriteAllText(file, text);
+            }
+            else { Console.WriteLine("U wrong,i cant find this word!"); }
+        }
+
+        public void Search(Dictionary<string, string> dic)  //Метод позволяет найти слово ,запрашивая у пользователя слово которое нужно найти
+        {
+            if (dic != null)
+            {
+                Console.Write("Enter word you want to search: ");
+                string word = Console.ReadLine();
+                if (dic.ContainsKey(word))
+                {
+                    Console.WriteLine($"{word} - {dic[word]}");
+                }
+                else
+                {
+                    foreach (var key in dic.Keys)
+                    {
+                        if (dic[key] == word)
+                        {
+                            Console.WriteLine($"{key} - {dic[key]}");
+                        }
+                    }
+                }
+            }
+        }
+
+        public void Print()  //Финальный метод выводит содержимое словаря в консоль используя StreamReader для чтения из файла
+        {
+            Console.WriteLine($"  Type: {type}");
+            StreamReader sr = new StreamReader(type + ".txt", Encoding.UTF8);
+            Console.WriteLine(sr.ReadToEnd());
+            sr.Close();
+        }
+
+    }
+}
